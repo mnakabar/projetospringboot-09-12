536dd53d7bbf36f516acc1ed7f807e7f99b712f8
Mauricio <mauricio.nakabar@mercadolivre.com>
2021-12-09 16:33:23 -0300

    projeto springboot

---
+++ src/main/java/com/developer/rojetospringboot/RojetospringbootApplication.java
@@ -0,0 +1,13 @@
+package com.developer.rojetospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RojetospringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RojetospringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/developer/rojetospringboot/RojetospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.rojetospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RojetospringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
